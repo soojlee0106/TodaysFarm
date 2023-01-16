c9733f9b6a76b31d2bd5c13a7ca0960108aa37dd
soojlee0106 <soojlee0106@naver.com>
2023-01-16 11:09:26 +0900

    Finish initial website

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <p id="photo-icon"></p>
       <header className="App-header">
         <p>
-          Today's Farm
+          Today's <br></br>
+          Farm
         </p>
       </header>
+      <p id="chatting"><p id="location"><img id="location-icon" src="/images/location-icon.png" alt="location-icon" /></p></p>
     </div>
   );
 }
