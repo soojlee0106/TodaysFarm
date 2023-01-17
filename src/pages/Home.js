ec8ac0fe0913c1a6c0d776ae3e71961acb80813c
soojlee0106 <soojlee0106@naver.com>
2023-01-17 11:23:10 +0900

    Carousel 추가

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,14 @@
 import '../App.css';
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import icon from "../images/location-icon.png"
+import carousel_1 from "../images/carousel_1.jpg"
+import carousel_2 from "../images/carousel_2.jpg"
+import carousel_3 from "../images/carousel_3.jpg"
+import carousel_4 from "../images/carousel_4.jpg"
+import carousel_5 from "../images/carousel_5.jpg"
+import carousel_6 from "../images/carousel_6.jpg"
+import Carousel from 'better-react-carousel'
 
 const Home = () => {
     const handleLogout = () => {
@@ -31,6 +38,33 @@ const Home = () => {
                     Farm
                 </p>
             </header>
+            <div className="Top-picks">
+                <h2>
+                    Top Picks
+                </h2>
+            </div>
+            <div className="Carousel-items">
+                <Carousel cols={3} rows={1} gap={10} loop>
+                    <Carousel.Item>
+                        <img id="carousel-left" width="100%" src={carousel_1} />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img width="100%" src={carousel_2} />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img id="carousel-right" width="100%" src={carousel_3} />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img id="carousel-left" width="100%" src={carousel_4} />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img width="100%" src={carousel_5} />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img id="carousel-right" width="100%" src={carousel_6} />
+                    </Carousel.Item>
+                </Carousel>
+            </div>
             <div id="chatting"><p id="location"><img id="location-icon" src={icon} alt="location-icon" /></p></div>
         </div>
 
