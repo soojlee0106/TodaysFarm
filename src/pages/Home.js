b24ab1dfac0d9279fcd0e01ec609a99175d6d609
soojlee0106 <soojlee0106@naver.com>
2023-01-19 11:44:57 +0900

    현재 날씨 보여주기

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,13 +9,18 @@ import carousel_4 from "../images/carousel_4.jpg"
 import carousel_5 from "../images/carousel_5.jpg"
 import carousel_6 from "../images/carousel_6.jpg"
 import Carousel from 'better-react-carousel'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSun, faCloud } from '@fortawesome/free-solid-svg-icons'
 
 const Home = () => {
     const handleLogout = () => {
         sessionStorage.removeItem('Auth Token');
         navigate('/')
     }
+    const [weather, setWeather] = useState(null);
+    const [location, setLocation] = useState(null);
     let navigate = useNavigate();
+
     useEffect(() => {
         let authToken = sessionStorage.getItem('Auth Token')
 
@@ -26,12 +31,33 @@ const Home = () => {
         if (!authToken) {
             navigate('/')
         }
+
+        navigator.geolocation.getCurrentPosition(async position => {
+            const { latitude, longitude } = position.coords;
+            const apiKey = 'a7292da2a85fdd4ef917687eac901e5e';
+            const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+            const response = await fetch(apiUrl);
+            const data = await response.json();
+            setWeather(data.weather[0].main);
+            setLocation(data.name);
+        });
+
     }, [])
 
     return (
         <div className="App">
             <button id="logout-button" onClick={handleLogout}>Log out</button>
             <p id="photo-icon"></p>
+            <div id="weather-text">
+                {location}<br></br>
+            </div>
+            <div id="weather-icon">
+                {weather === 'Clear' ? (
+                    <FontAwesomeIcon icon={faSun} />
+                ) : (
+                    <FontAwesomeIcon icon={faCloud} />
+                )}
+            </div>
             <header className="App-header">
                 <p>
                     Today's <br></br>
@@ -46,22 +72,22 @@ const Home = () => {
             <div className="Carousel-items">
                 <Carousel cols={3} rows={1} gap={10} loop>
                     <Carousel.Item>
-                        <img id="carousel-left" width="100%" src={carousel_1} />
+                        <img id="carousel-left" width="100%" src={carousel_1} alt="c1" />
                     </Carousel.Item>
                     <Carousel.Item>
-                        <img width="100%" src={carousel_2} />
+                        <img width="100%" src={carousel_2} alt="c2" />
                     </Carousel.Item>
                     <Carousel.Item>
-                        <img id="carousel-right" width="100%" src={carousel_3} />
+                        <img id="carousel-right" width="100%" src={carousel_3} alt="c3" />
                     </Carousel.Item>
                     <Carousel.Item>
-                        <img id="carousel-left" width="100%" src={carousel_4} />
+                        <img id="carousel-left" width="100%" src={carousel_4} alt="c4" />
                     </Carousel.Item>
                     <Carousel.Item>
-                        <img width="100%" src={carousel_5} />
+                        <img width="100%" src={carousel_5} alt="c5" />
                     </Carousel.Item>
                     <Carousel.Item>
-                        <img id="carousel-right" width="100%" src={carousel_6} />
+                        <img id="carousel-right" width="100%" src={carousel_6} alt="c6" />
                     </Carousel.Item>
                 </Carousel>
             </div>
